7b00bc3587d4caa9c831c0ee08980387cf152577
Toni Fey <ToniFey@protonmail.com>
2025-10-21 10:35:35 +0200

    removed filter settings

--- .gitattributes.d/clean_secrets.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-sed -E 's/(#define WIFI_SSID )".*"/\1"YOUR_WIFI_SSID"/' | \
-sed -E 's/(#define WIFI_PASSWORD )".*"/\1"YOUR_WIFI_PASSWORD"/'
--- .gitattributes.d/smudge_secrets.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-sed -E 's/(#define WIFI_SSID )".*"/\1"Vodafone-05FD"/' | \
-sed -E 's/(#define WIFI_PASSWORD )".*"/\1"tmgahUGh4rPHT34Q"/'
