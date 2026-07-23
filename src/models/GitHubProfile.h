c6e2155e6939eae9a81e1e745dd4e19c0b6cbb03
Toni Fey <ToniFey@protonmail.com>
2026-07-23 20:28:30 +0200

    feat: Extend GitHubProfile struct with additional fields for bio, blog, social media, and repository details

--- src/models/GitHubProfile.h
+++ src/models/GitHubProfile.h
@@ -13,6 +13,19 @@ struct GitHubProfile
 {
     String username;
     String name;
+    String bio;
+    String blog;
+    String twitterUsername;
+    String email;
+    String company;
+    String* followerList;
+    String* followingList;
+    String* repos;
+    String* starredRepos;
+    int publicRepos;
+    int publicGists;
     int followers;
     int following;
+    int stars;
+    int starred;
 };
