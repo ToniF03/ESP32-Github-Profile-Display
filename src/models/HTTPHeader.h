f81ef7da9d4ad4a561470f50267f502897243a81
Toni Fey <ToniFey@protonmail.com>
2026-07-26 05:19:12 +0200

    docs: Update description in HTTPHeader.h for clarity and context

--- src/models/HTTPHeader.h
+++ src/models/HTTPHeader.h
@@ -2,7 +2,10 @@
  * Created on: 2026-07-25
  * Author(s): Toni Fey
  * License: MIT
- * Description: 
+ * Description: Represents a single HTTP header (key/value pair) used when
+ *              sending or parsing HTTP requests and responses. This lightweight
+ *              struct is intended for use with Arduino/ESP32 networking
+ *              utilities within the project.
  */
 
 #pragma once
